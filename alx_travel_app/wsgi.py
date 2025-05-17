241d1b54c859583b6cccd9f04a9f64602324d24b
karivone <karimiyvonne19@gmail.com>
2025-05-17 12:31:39 +0300

    app

--- alx_travel_app/wsgi.py
+++ alx_travel_app/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
 
 application = get_wsgi_application()
+
+//checking kama kuna push
